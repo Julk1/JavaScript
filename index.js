a3f0da16fb01b70f05c0662ee37e9b4d298fbfc1
Julk1 <babailova.j.b@gmail.com>
2023-03-11 18:23:24 +0100

    lesson21

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let a = prompt('Enter the first number', 0);
 let b = prompt('Enter the second number', 0);
 
-alert('sum is' + " " +  (Number(a) + Number(b)));
-alert('difference is' + " " +  (a - b));
-alert('product is' + " " + (a * b));
-alert('quotient is' + " " + (a / b));
-alert('Thank you!');
+alert('Sum is' + " " +  (Number(a) + Number(b)));
+alert('Difference is' + " " +  (a - b));
+alert('Product is' + " " + (a * b));
+alert('Quotient is' + " " + (a / b));
+alert('Thank you :)!');

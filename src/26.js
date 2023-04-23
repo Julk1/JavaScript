f8e49b8909d860973e8b31befed85621eba0ff89
Julk1 <babailova.j.b@gmail.com>
2023-04-23 16:58:07 +0200

    less30

---
+++ src/26.js
@@ -0,0 +1,35 @@
+
+
+function Accumulator(startingValue) {
+
+    if (typeof startingValue !== "number") {
+        throw new Error("Value must be a number");
+      }
+
+    this.value = startingValue;
+
+}
+  
+    Accumulator.prototype.increment = function(){
+      this.value++;
+    };
+  
+    Accumulator.prototype.decrement = function() {
+      this.value--;
+    };
+  
+
+
+
+function CancelableAccumulator(startingValue) {
+    Accumulator.call(this, startingValue);
+    this.startingValue = startingValue;
+ }
+
+CancelableAccumulator.prototype = Object.create(Accumulator.prototype);
+CancelableAccumulator.prototype.constructor = CancelableAccumulator;
+
+CancelableAccumulator.prototype.clear = function() {
+  this.value = this.startingValue;
+};
+
